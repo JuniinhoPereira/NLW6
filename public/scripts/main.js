06726728d864460255f89b426d1f9f3795f80792
JuniinhoPereira <juniinho.pereira1@gmail.com>
2021-07-02 15:30:42 -0300

    iniciando sql

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -25,16 +25,17 @@ function handleClick(event, check = true){
     event.preventDefault()
     const text = check ? "Marcar como lida" : "Excluir"
     const slug = check ? "check" : "delete"
-
     const roomId = document.querySelector("#room-id").dataset.id
+    const questionId = event.target.dataset.id
 
     const form = document.querySelector(".modal form")
-    form.setAttribute("action", `/room/${roomId}/:question/:action`)
+    form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`)
 
     modalTitle.innerHTML= `${text} esta pergunta`
     modalDescription.innerHTML= `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
     modalButton.innerHTML= `Sim, ${text.toLowerCase()}`
     check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
     // Abrir a modal
     modal.open()
 }
